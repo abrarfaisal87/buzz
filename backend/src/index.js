f14c32c54dda236425997e704cd75ccba1fc5984
unknown <abrarfaisal87@gmail.com>
2025-02-12 13:34:48 +0600

    stupid

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,10 +19,11 @@ app.use(express.json())//for parsing app/json
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
-if(process.env.NODE_ENV !== "development"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV !== "development") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));  // Go up one level to find frontend
+    
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
     });
 }
 
